ba1dcd3326f5ce77268d88c569630c80184b2077
mcerjak <mcerjak@85.cerfrance.fr>
2023-02-06 16:18:45 +0100

    first commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+
+const Header = () => {
+    const [menu, setMenu] = useState(false);
+
+    const handleMenu = () => {
+        setMenu(!menu);
+    };
+
+    return (
+        <div>
+            <header className="header">
+                <div className="header-top">
+                    <div className="header-left">
+                        <h1>48h</h1>
+                    </div>
+                    <div className="header-center">
+                        <input type="text" placeholder="Search" className="search-bar"/>
+                    </div>
+                    <div className="header-right">
+                        <button className="bg-b">Sign in</button>
+                        <button className="bg-b">Sign up</button>
+                    </div>
+                </div>
+                <div className="header-bottom">
+                    <button className="burger-menu-btn" onClick={handleMenu}>Menu</button>
+                    <nav>
+                        <ul className="navbar">
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">About</a></li>
+                            <li><a href="#">Contact</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
+            <div className={ menu ? "side-menu active" : "side-menu"}>
+                <ul className="side-menu-list">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
